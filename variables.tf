ff4e06d5775f15b05f6f9bb73e6f66d4acad1bc2
surendra <surihcl07@gmail.com>
2022-09-13 17:45:12 +0530

    intial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ecr_name"{
+  description = "ECR name"
+  type        = string
+  default     = "aap2-ecr"
+
+}
